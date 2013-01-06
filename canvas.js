0e2214beb2e8331ab19749edcb569a46e6bf72ff
David Albert <davidbalbert@gmail.com>
2013-01-06 14:20:59 -0500

    Rounded rects

--- canvas.js
+++ canvas.js
@@ -2,29 +2,21 @@ function draw() {
   var canvas = document.getElementById("c");
   var ctx = canvas.getContext("2d");
 
-  /*
-  ctx.save();
-  ctx.translate(50, 25);
-  ctx.rotate(Math.PI / 4);
-  roundRect(ctx, 0, 0, 50, 50, 0);
-  ctx.restore();
-  */
+  roundRect2(ctx, 25, 25, 100, 50, 0);
+  roundRect2(ctx, 25, 100, 100, 50, 15);
+  roundRect2(ctx, 25, 175, 100, 50, 25);
+
+  roundRect2(ctx, 25, 250, 50, 50, 25);
 
   /*
-  roundRect(ctx, 25, 25, 100, 50, 0);
-  roundRect(ctx, 25, 100, 100, 50, 15);
-  roundRect(ctx, 25, 175, 100, 50, 25);
-
-  roundRect(ctx, 25, 250, 50, 50, 25);
-  */
-
   for (var i = 0; i < 16; i++) {
-    squareWithRotation2(ctx, 50, 50, 200, Math.PI / 32 * i);
+    squareWithRotation(ctx, 50, 50, 200, Math.PI / 32 * i);
   }
+  */
 
 }
 
-function squareWithRotation2(ctx, x, y, side, angle) {
+function squareWithRotation(ctx, x, y, side, angle) {
   ctx.save();
   ctx.translate(x + side / 2, y + side / 2);
   ctx.rotate(angle);
@@ -35,20 +27,6 @@ function squareWithRotation2(ctx, x, y, side, angle) {
   ctx.lineTo(side, side);
   ctx.lineTo(0, side);
   ctx.lineTo(0, 0);
-  ctx.stroke();
-  ctx.restore();
-}
-
-function squareWithRotation(ctx, x, y, side, angle) {
-  ctx.save();
-  ctx.translate(x + side / 2, y + side / 2);
-  ctx.rotate(angle);
-  ctx.beginPath();
-  ctx.moveTo(-side / 2, -side / 2);
-  ctx.lineTo(side / 2, - side / 2);
-  ctx.lineTo(side / 2, side / 2);
-  ctx.lineTo(- side / 2, side / 2);
-  ctx.lineTo(- side / 2, - side / 2);
   ctx.stroke();
   ctx.restore();
 }
@@ -80,5 +58,8 @@ function roundRect2(ctx, x, y, width, height, radius) {
   ctx.lineTo(x + width, y + height - radius);
   ctx.arc(x + width - radius, y + height - radius, radius, 0, Math.PI * 0.5, false);
   ctx.lineTo(x + radius, y + height);
+  ctx.arc(x + radius, y + height - radius, radius, Math.PI * 0.5, Math.PI, false);
+  ctx.lineTo(x, y + radius);
+  ctx.arc(x + radius, y + radius, radius, Math.PI, Math.PI * 1.5, false);
   ctx.stroke();
 }
